8619db4b946884119f6f69d217a4beea25edebb2
breduarm <bryan.e.armijos@gmail.com>
2024-07-04 23:53:48 -0500

    Add theming

---
+++ src/theme/ColorSqueme.ts
@@ -0,0 +1,12 @@
+const Colors = {
+    primary: '#F9DE4A',
+    onPrimary: '#303034',
+    secondary: '#E9ECF2',
+    onSecondary: '#2B3B69',
+    error: '#C32B1F',
+    onError: '#FFFFFF',
+    background: '#FFFFFF',
+    onBackground: '#303034',
+  };
+  
+  export default Colors;
