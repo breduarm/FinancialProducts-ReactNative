03a35d486cdfc3e19f15c5c76dbd71bc73b8d0b0
breduarm <bryan.e.armijos@gmail.com>
2024-07-08 12:00:00 -0500

    Custom header styles

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,18 +3,31 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import DetailScreen from '../screens/DetailScreen';
-import { NavDirections } from '../enums/NavDirections';
+import {NavDirections} from '../enums/NavDirections';
 import ProductFormScreen from '../screens/ProductFormScreen';
+import Colors from '../theme/ColorSqueme';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = (): React.JSX.Element => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={NavDirections.HOME}>
+      <Stack.Navigator
+        initialRouteName={NavDirections.HOME}
+        screenOptions={{
+          title: 'Banco',
+          headerTitleAlign: 'center',
+          headerTintColor: Colors.onSecondary,
+          headerTitleStyle: {
+            fontWeight: '700',
+          },
+        }}>
         <Stack.Screen name={NavDirections.HOME} component={HomeScreen} />
         <Stack.Screen name={NavDirections.DETAIL} component={DetailScreen} />
-        <Stack.Screen name={NavDirections.NEW_PRODUCT} component={ProductFormScreen} />
+        <Stack.Screen
+          name={NavDirections.NEW_PRODUCT}
+          component={ProductFormScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
